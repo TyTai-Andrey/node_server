6c4c7168d521fd36fb4f61e494f3c1c99e9d1f7c
TyTai-Andrey <andriynov-99@mail.ru>
2022-02-02 18:49:37 +0300

    nodeServer

---
+++ README.md
@@ -0,0 +1,9 @@
+### `how starting?`
+
+### `npm i for client`
+
+### `npm i for server`
+
+### `npm start for server`
+
+### `npm start for client`
---
+++ routes/post.routes.js
@@ -0,0 +1,54 @@
+const { Router } = require('express');
+const config = require('config');
+// const shortid = require('shortid');
+const User = require('../models/User');
+const Post = require('../models/Post');
+const auth = require('../middleware/auth.middleware');
+const router = Router();
+
+router.post('/generate', auth, async (req, res) => {
+  try {
+    const baseUrl = config.get('baseUrl');
+    const { title, text } = req.body;
+
+    // const code = shortid.generate();
+    console.log(req.user.userId);
+
+    const candidate = await User.findById(req.user.userId);
+
+    if (candidate.role !== 'admin') {
+      return res.json({ message: 'Вы не администратор' });
+    }
+
+    const post = new Post({
+      title,
+      text,
+      owner: req.user.userId,
+    });
+
+    await post.save();
+    res.status(201).json({ post });
+  } catch (e) {
+    res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' });
+  }
+});
+
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find({ owner: req.user.userId });
+    res.json(posts);
+  } catch (e) {
+    res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' });
+  }
+});
+
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.json(post);
+  } catch (e) {
+    res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' });
+  }
+});
+
+module.exports = router;
